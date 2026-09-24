44956d522c936af434dc75ad4f302b0bc9e345b4
smiledoneholy <smiledoneholy95@gmail.com>
2026-09-25 00:11:58 +0100

    Home page replacement

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   async redirects() {
     return [
+      { source: '/iptv-subscription', destination: '/', permanent: true },
       { source: '/home', destination: '/', permanent: true },
       { source: '/faq', destination: '/#faq', permanent: true },
       { source: '/channel-list', destination: '/channels', permanent: true },
